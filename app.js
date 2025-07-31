b9895457e1a9a278da325e2ad1d12b183207a29c
Filip Březík <filip.brezik@email.cz>
2025-07-31 17:24:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function scrollButtonhome() {
+    document.getElementById("mainPage").scrollIntoView({behavior:"smooth"});
+  }
+
+function scrollButton() {
+    document.getElementById("firstSection").scrollIntoView({behavior:"smooth"});
+  }
+
+function scrollButtonNews() {
+    document.getElementById("newsPage").scrollIntoView({behavior:"smooth"});
+  }
+
+function scrollButtonClubs() {
+    document.getElementById("clubsPage").scrollIntoView({behavior:"smooth"});
+  }
+
+function scrollButtonSports() {
+    document.getElementById("sportsPage").scrollIntoView({behavior:"smooth"});
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html',
+    './public/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sequel-sans': ['Sequel Sans', 'sans-serif'],
+        'darkline': ['Darkline', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
